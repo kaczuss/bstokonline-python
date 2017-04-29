7cfaa98e79466adcbd68c44145d856e4d3196dcd
Pawel Kaczanowski <pawel.kaczanowski@cronn.de>
2017-04-29 12:34:37 +0200

    read data from page and parse

--- offers_finder.py
+++ offers_finder.py
@@ -1,6 +1,13 @@
+from urllib.request import urlopen
+
 from bs4 import BeautifulSoup
 
 class OffersFinder(object):
+
+    def get_latest_offers(self):
+        url = 'http://www.bialystokonline.pl/domy-mieszkania-sprzedam,ogloszenia,5,1.html'
+        data = urlopen(url).read()
+        return self.get_offers(data)
 
     def get_offers(self, html):
         soup = BeautifulSoup(html, 'html.parser')
--- test_offersFinder.py
+++ test_offersFinder.py
@@ -1,3 +1,4 @@
+import mock
 from unittest import TestCase
 
 from offers_finder import OffersFinder
@@ -8,3 +9,20 @@ class TestOffersFinder(TestCase):
         finder = OffersFinder()
         offers = finder.get_offers(open('sample.html', 'r'))
         self.assertEqual(offers.__len__(), 27)
+
+    def side_effect(self, arg):
+        values = {'http://www.bialystokonline.pl/domy-mieszkania-sprzedam,ogloszenia,5,1.html': open('sample.html', 'r'),
+                 'c': 3}
+        return values[arg]
+
+
+    @mock.patch('offers_finder.urlopen')
+    def test_get_latest_offers(self, url_open):
+        url_open.side_effect = self.side_effect
+        finder = OffersFinder()
+        offers = finder.get_latest_offers()
+        url_open.assert_called_with('http://www.bialystokonline.pl/domy-mieszkania-sprzedam,ogloszenia,5,1.html')
+        self.assertEqual(offers.__len__(), 27)
+
+
+
