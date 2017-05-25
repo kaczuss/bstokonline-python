601888fde36e5106510d2da59a57976884a726e3
Pawel Kaczanowski <pawel.kaczanowski@cronn.de>
2017-05-25 21:38:44 +0200

    add some filters

--- app.py
+++ app.py
@@ -11,6 +11,25 @@ from storage import OffersStorage
 from offers_finder import OffersFinder
 
 
+def filtered(offer):
+    forbidden_words = ['kawalerk', 'sokolka', 'sokółka', 'sokólka', 'lewickie', 'juchnowiec', 'zabłudów', 'zabludow',
+                       'zabłudów', 'zabludów']
+    for word in forbidden_words:
+        if word in offer.title.lower():
+            return True
+        if word in offer.description.lower():
+            return True
+
+    return False
+
+
+def is_new_offer(new_offer, offers_to_ignore):
+    for old_offer in offers_to_ignore:
+        if old_offer['title'] == new_offer.title:
+            return False
+    return True
+
+
 def run():
     storage = OffersStorage()
     last_offer_date = storage.find_latest_date()
@@ -19,22 +38,18 @@ def run():
     finder = OffersFinder()
     offers = finder.get_latest_offers(last_offer_date)
 
-    def is_new_offer(new_offer, offers_to_ignore):
-        for old_offer in offers_to_ignore:
-            if old_offer['title'] == new_offer.title:
-                return False
-        return True
-
     print('found new {} offers'.format(offers.__len__()))
     for offer in offers:
-        if is_new_offer(offer, old_offers):
+        if not is_new_offer(offer, old_offers):
+            print('offer already added {} with title {}'.format(offer._id, offer.title))
+        elif filtered(offer):
+            print('offer {} was filtered with title {}'.format(offer._id, offer.title))
+        else:
             Trello().add_offers([offer])
             added_offer = storage.store(offer)
             old_offers.append(added_offer)
             print('offer added {} with title {}'.format(offer._id, offer.title))
-        else:
-            print('offer already added {} with title {}'.format(offer._id, offer.title))
-
+            
     print("done")
 
 
