fe9148497b2e947dc6c40022d5cc0eda8735b878
Pawel Kaczanowski <pawel.kaczanowski@cronn.de>
2017-06-06 14:44:04 +0200

    remove some forbidden words

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ def filtered_words(offer):
     forbidden_words = ['kawalerk', 'sokolka', 'sokółka', 'sokólka', 'lewickie', 'juchnowiec', 'zabłudów', 'zabludow',
                        'zabłudów', 'zabludów', 'rajgrod', 'rajgród', '535-536-005', 'izabelin', 'ełk', 'elk',
                        'siedlisko', 'gródek', 'grodek', 'gródku', 'grodku', 'ciasne', 'niewodnica', 'niewodnicy',
-                       'antresol', 'solniczki', 'kurian', 'dobrzyniewo', 'augustowie', 'augustów', 'choroszcz',
-                       'hajnówka', 'hajnówce', '2 pokoje', '2  pokoje', 'parter', 'dwupokojowe', '2pok', '2-pok']
+                       'solniczki', 'kurian', 'dobrzyniewo', 'augustowie', 'augustów', 'choroszcz',
+                       'hajnówka', 'hajnówce', '2 pokoje', '2  pokoje', 'dwupokojowe', '2pok', '2-pok']
     for word in forbidden_words:
         if word in offer.title.lower():
             print('forbidden word in title is {}'.format(word))
