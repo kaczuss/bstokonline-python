009c1107502ad40cd0665c5a1e0bbf48367e4a7e
Pawel Kaczanowski <pawel.kaczanowski@cronn.de>
2017-06-21 19:23:07 +0200

    add script to run on server

---
+++ docker_scripts/deploy_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+set +e
+docker stop $(docker ps  -f ancestor=ogloszenia -q)
+set -e
+mkdir -p /app
+cd /app
+
+tar xvf dockerized.tar
+rm dockerized.tar
+
+docker build -t ogloszenia .
+
+docker run -d -e bstok_env='prod' ogloszenia
+
